bda7bd80b07a3275ed0574691e49537c6cbc6b68
abhinandan-verma <abhinandanverma551@gmail.com>
2024-09-29 13:58:23 +0530

    turbo repo setup complete

--- config/seo.ts
+++ config/seo.ts
@@ -1,12 +1,12 @@
 // src/config/seo.ts
 export const SEO = {
-    title: 'Caresept - Bridging Communication Gaps',
-    description: 'Caresept bridges the communication gap between customers in the EU & US and companies providing services/products to them.',
-    openGraph: {
-      type: 'website',
-      locale: 'en_US',
-      url: 'https://caresept.com/',
-      site_name: 'Caresept',
-    },
-  };
-  
+  title: 'Caresept - Bridging Communication Gaps',
+  description:
+    'Caresept bridges the communication gap between customers in the EU & US and companies providing services/products to them.',
+  openGraph: {
+    type: 'website',
+    locale: 'en_US',
+    url: 'https://caresept.com/',
+    site_name: 'Caresept',
+  },
+};
